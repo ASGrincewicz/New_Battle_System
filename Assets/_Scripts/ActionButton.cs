45bcc0ab34b24a0a6ef6de3dba257c8f78bbd28a
Aaron Grincewicz <asgrincewicz@icloud.com>
2022-02-13 23:37:21 -0500

    button setup

---
+++ Assets/_Scripts/ActionButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionButton : MonoBehaviour
+{
+    public enum ButtonPos { Top_Left, Top_Right, Bottom_Left, Bottom_Right }
+    [Tooltip("Assign the button's position here.")]
+    [SerializeField] private ButtonPos _buttonPosition;
+
+    private void OnEnable()
+    {
+        switch (_buttonPosition)
+        {
+            case ButtonPos.Top_Left:
+                transform.GetComponentInChildren<Text>().text = "Attack";
+                break;
+            case ButtonPos.Top_Right:
+                transform.GetComponentInChildren<Text>().text = "Defend";
+                break;
+            case ButtonPos.Bottom_Left:
+                transform.GetComponentInChildren<Text>().text = "Item";
+                break;
+            case ButtonPos.Bottom_Right:
+                transform.GetComponentInChildren<Text>().text = "Escape";
+                break;
+        }
+    }
+
+}
